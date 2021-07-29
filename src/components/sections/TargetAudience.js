acd03cdea041cfcd660e25dda360fe40ac9ae72c
Sophie_Z <68120193+Sophie-Xinyu-Zhang@users.noreply.github.com>
2021-07-29 02:49:45 -0400

    added target audience component, styling of the list to be fixed, content needs to be filled

---
+++ src/components/sections/TargetAudience.js
@@ -0,0 +1,123 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionSplitProps.types,
+};
+
+const defaultProps = {
+  ...SectionSplitProps.defaults,
+};
+
+const TargetAudience = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "features-split section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "features-split-inner section-inner",
+    "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const splitClasses = classNames(
+    "split-wrap",
+    invertMobile && "invert-mobile",
+    invertDesktop && "invert-desktop",
+    alignTop && "align-top"
+  );
+
+  const sectionHeader = {
+    title: "Who is this program for and not for.",
+    // paragraph:
+    //   "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+            <div className="split-item divide-x-2 divide-green-500 center-content-mobile center-content">
+              <div
+                className="split-item-content reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                {/* <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Guide One
+                </div> */}
+                <h3 className="text-color-primary mt-0 mb-12">For</h3>
+                <ul className="m-0 list-inside">
+                  <li>Characteristic one</li>
+                  <li>Characteristic two</li>
+                  <li>Characteristic three</li>
+                  <li>Characteristic four</li>
+                </ul>
+              </div>
+
+              <div
+                className="split-item-content reveal-from-right"
+                data-reveal-container=".split-item"
+              >
+                {/* <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Guide Two
+                </div> */}
+                <h3 className="text-color-primary mt-0 mb-12">Not For</h3>
+                <ul className="m-0 list-inside text-center">
+                  <li>Characteristic one</li>
+                  <li>Characteristic two</li>
+                  <li>Characteristic three</li>
+                  <li>Characteristic four</li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <div className="split-item">
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-02.png")}
+                  alt="Features split 02"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+TargetAudience.propTypes = propTypes;
+TargetAudience.defaultProps = defaultProps;
+
+export default TargetAudience;
--- src/views/Home.js
+++ src/views/Home.js
@@ -4,6 +4,7 @@ import Hero from "../components/sections/Hero";
 import AboutTheProgram from "../components/sections/AboutTheProgram";
 import WhoAreWe from "../components/sections/WhoAreWe";
 import Testimonial from "../components/sections/Testimonial";
+import TargetAudience from "../components/sections/TargetAudience";
 import Cta from "../components/sections/Cta";
 
 const Home = () => {
@@ -17,6 +18,7 @@ const Home = () => {
         imageFill
         className="illustration-section-02"
       />
+      <TargetAudience />
       <Testimonial topDivider />
       <Cta split />
     </>
